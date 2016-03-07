f5fef192d8a3d9c1d270c7f40bb7b1a8c2af5dbb
MetanoKid <carloscdcskate@gmail.com>
2016-03-07 21:34:55 +0100

    Adds separated cards by type in player

--- src/main/players/Player.scala
+++ src/main/players/Player.scala
@@ -1,6 +1,9 @@
 package main.players
 
 import main.cards.Card
+import main.cards.SuspectCard
+import main.cards.WeaponCard
+import main.cards.RoomCard
 
 object Player {
   val Empty = new Player(id = -1, cards = List())
@@ -8,6 +11,10 @@ object Player {
 
 class Player(val id: Int, val cards: List[Card]) {  
   override def toString = s"Player[id=$id, cards=$cards]"
+  
+  lazy val suspects = cards.filter { card => card.isInstanceOf[SuspectCard] }
+  lazy val weapons  = cards.filter { card => card.isInstanceOf[WeaponCard] }
+  lazy val rooms    = cards.filter { card => card.isInstanceOf[RoomCard] }
   
   def dealCard(card: Card) =
     new Player(id = id, cards = card :: cards)
--- src/test/PlayerSpec.scala
+++ src/test/PlayerSpec.scala
@@ -3,22 +3,37 @@ package test
 import org.scalatest.FlatSpec
 import main.cards.ReverendGreen
 import main.players.Player
+import main.cards.BilliardRoom
+import main.cards.LeadPipe
 
 class PlayerSpec extends FlatSpec {
   behavior of "A Player"
   
+  val player = Player.Empty
+  
   it should "have an extra card when dealing a card" in {
     val card = ReverendGreen
-    val basePlayer = Player.Empty
-    val player = basePlayer.dealCard(card)
+    val playerWithCard = player.dealCard(card)
     
-    assert(player.cards.size == basePlayer.cards.size + 1)
+    assert(playerWithCard.cards.size == player.cards.size + 1)
   }
   
   it should "have dealt card in its hand" in {
     val card = ReverendGreen
-    val player = Player.Empty.dealCard(card)
+    val playerWithCard = player.dealCard(card)
     
-    assert(player.cards.contains(card))
+    assert(playerWithCard.cards.contains(card))
+  }
+  
+  it should "have correctly separated cards by type" in {
+    val suspect = ReverendGreen
+    val weapon = LeadPipe
+    val room = BilliardRoom
+    val playerWithCards = player.dealCard(suspect).dealCard(weapon).dealCard(room)
+    
+    assert(playerWithCards.cards.size == playerWithCards.suspects.size + playerWithCards.weapons.size + playerWithCards.rooms.size)
+    assert(playerWithCards.suspects.size == 1 && playerWithCards.suspects.contains(suspect))
+    assert(playerWithCards.weapons.size == 1 && playerWithCards.weapons.contains(weapon))
+    assert(playerWithCards.rooms.size == 1 && playerWithCards.rooms.contains(room))
   }
 }
