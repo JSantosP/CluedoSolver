e70e4436c3cc4f4bfcf146efe6cc142b4f5d9649
MetanoKid <carloscdcskate@gmail.com>
2016-03-09 14:33:51 +0100

    Adds test for updated knowledge after a card is dealt

--- src/test/knowledge/KnowledgeSpec.scala
+++ src/test/knowledge/KnowledgeSpec.scala
@@ -1,21 +1,18 @@
 package test.knowledge
 
-import org.scalatest.BeforeAndAfter
 import org.scalatest.FlatSpec
 
+import main.cards.ReverendGreen
 import main.game.Game
+import main.players.Player
 import main.players.knowledge.Knowledge
 
-class KnowledgeSpec extends FlatSpec with BeforeAndAfter {
+class KnowledgeSpec extends FlatSpec {
   behavior of "An initialized Knowledge"
   
-  var knowledge = Knowledge.Empty
   val numPlayers = Game.Empty.numPlayers
   val cards = Game.Empty.cards
-  
-  before {
-    knowledge = knowledge.initialize(numPlayers, cards)
-  }
+  val knowledge = Knowledge.Empty.initialize(numPlayers, cards)
   
   it should "have as many SingleKnowledge pieces as players" in {
     assert(knowledge.players.size == numPlayers)
@@ -26,10 +23,28 @@ class KnowledgeSpec extends FlatSpec with BeforeAndAfter {
       (knowledge, index) <- knowledge.players.zipWithIndex
     } yield {
       knowledge.possibleCards.size == cards.size
-    }).forall { playerHasAllPossibleCards => playerHasAllPossibleCards == true }
+    }).forall { playerHasAllPossibleCards => playerHasAllPossibleCards }
     
     val envelopeWithAllPossibleCards = knowledge.envelope.possibleCards.size == cards.size
     
     assert(allPlayersWithAllPossibleCards && envelopeWithAllPossibleCards)
+  }
+  
+  it should "have updated SingleKnowledges when a card is dealt to a player" in {
+    val playerToTest = Player.Empty.withID(0)
+    val cardToTest = ReverendGreen
+    
+    val updatedKnowledge = knowledge.playerHasCard(playerToTest, cardToTest)
+    
+    val allPlayersCorrectlyUpdated = (for {
+      (singleKnowledge, index) <- updatedKnowledge.players.zipWithIndex
+    } yield {
+      if(index == playerToTest.id) singleKnowledge.cards.contains(cardToTest) && !singleKnowledge.possibleCards.contains(cardToTest)
+      else !singleKnowledge.possibleCards.contains(cardToTest)
+    }).forall { isPlayerCorrect => isPlayerCorrect }
+    
+    val envelopeCorrectlyUpdated = !updatedKnowledge.envelope.possibleCards.contains(cardToTest)
+    
+    assert(allPlayersCorrectlyUpdated && envelopeCorrectlyUpdated)
   }
 }
